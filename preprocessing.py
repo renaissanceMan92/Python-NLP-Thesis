257ff3e34be83146a1e7484ee31266a604c629d9
Robin Holm <49246098+renaissanceMan92@users.noreply.github.com>
2021-04-19 11:25:36 +0200

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -8,14 +8,15 @@ from gensim import corpora
 from gensim.models import Phrases
 from gensim.corpora import Dictionary
 
-print('\nImporting csv file...')
+print('\nImporting csv file...') # Issue: x200b enters the corpus in this configuration.
 docs = []
-with open('posts.csv', encoding='ISO 8859-1') as file:
+with open('submissionsWithComments.csv', encoding='utf-8') as file:
     csv_reader = csv.reader(file, delimiter = '|')
     for row in csv_reader:
-        docs.append(row[0])
         try:
             docs.append(row[1])
+            docs.append(row[2])
+            docs.append(row[6])
         except IndexError:
             pass
 
@@ -28,20 +29,21 @@ docs = temp
 
 print('Tokenizing...')
 for idx in range(len(docs)):
-    docs[idx] = docs[idx].lower()  # Convert to lowercase.
-    docs[idx] = RegexpTokenizer(r'\w+').tokenize(docs[idx])  # Split into words.
+    docs[idx] = docs[idx].lower()
+    docs[idx] = RegexpTokenizer(r'\w+').tokenize(docs[idx])
 
-print('Removing all docs with less than 3 words...')
-docs = [doc for doc in docs if len(doc) > 4]
+print('Removing all docs with less than 4 words...')
+docs = [doc for doc in docs if len(doc) > 4] # Decision: less or more?
 
 print('Lemmatizing...')
 docs = [[WordNetLemmatizer().lemmatize(token) for token in doc] for doc in docs]
 
-print('Filtering for nouns and verbs...')
-docs = [[token for (token, pos) in nltk.pos_tag(doc) if pos[:2] == 'NN'
-or pos[:2] == 'VB'] for doc in docs]
+# Decision: POS-filtering, yes or no?
+#print('Filtering for nouns and verbs...')
+#docs = [[token for (token, pos) in nltk.pos_tag(doc) if pos[:2] == 'NN'
+#or pos[:2] == 'VB'] for doc in docs]
 
-print('Removing stop words etc...')
+print('Removing stop words, short words and non-alphanumeric words...')
 docs = [[token for token in doc if token not in set(stopwords.words('english'))
 and token.isalnum()
 and len(token) > 3] for doc in docs]
@@ -53,15 +55,10 @@ print('Creating dictionary...')
 dictionary = Dictionary(docs)
 
 print('Filtering extremes from dictionary...')
-dictionary.filter_extremes(no_below=20, no_above=0.5)
+dictionary.filter_extremes(no_below=20, no_above=0.5)# Decision: which values to use for filtering?
 
 print('Creating bag of words representation...')
 corpus = [dictionary.doc2bow(doc) for doc in docs]
-
-# Alternative TF-IDF:
-#from gensim import corpora, models
-#tfidf = models.TfidfModel(bow_corpus)
-#corpus_tfidf = tfidf[bow_corpus]
 
 print('Number of unique tokens: %d' % len(dictionary))
 print('Number of documents: %d' % len(corpus))
@@ -69,5 +66,8 @@ print('Number of documents: %d' % len(corpus))
 print('Saving dictionary and corpus to file...')
 dictionary.save('dictionary')
 corpora.MmCorpus.serialize('corpus', corpus)
+
+#print(docs)
+#print(corpus)
 
 print('Preprocessing complete. To perform topic modeling, run LDAmodeling.py.\n')
